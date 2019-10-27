01966a4a962de1a01012c5d18db77a465fc0ee0e
Florian Kopp <floriankopp@posteo.de>
2019-10-27 10:05:58 +0100

    Lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
+import logo from "./logo.svg"
 
-const App: React.FC = () => {
+const App: React.FC = () =>
+{
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +21,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
