294e88f65a5a9565b7573b79dd9bd17503ad8b14
Florian Kopp <floriankopp@posteo.de>
2019-10-27 11:04:43 +0100

    Implement initialize

---
+++ src/services/interactors/initialize.ts
@@ -0,0 +1,9 @@
+import { Board } from "../../domain/board";
+import { Coordinate } from "../../domain/coordinate";
+import { BoardInteractor } from './boardInteractor';
+
+export function initialize(size: Coordinate)
+{
+  const board = new Board(size)
+  return new BoardInteractor(board)
+}
