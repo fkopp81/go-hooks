3f37455b1f66f5b3cbf0cafb107a2702fcc3b993
Florian Kopp <floriankopp@posteo.de>
2019-10-27 11:04:14 +0100

    Implement interactors

---
+++ src/services/interactors/boardInteractor.ts
@@ -0,0 +1,14 @@
+import { Board } from "../../domain/board"
+import { Coordinate } from "../../domain/coordinate"
+import { FieldInteractor } from "./fieldInteractor";
+
+export class BoardInteractor
+{
+  constructor(public board: Board) { }
+
+  public fieldInteractor(coordinate: Coordinate)
+  {
+    const field = this.board.field(coordinate)
+    return new FieldInteractor(field)
+  }
+}
---
+++ src/services/interactors/fieldInteractor.ts
@@ -0,0 +1,6 @@
+import { Field } from "../../domain/field"
+
+export class FieldInteractor
+{
+  constructor(public field: Field) { }
+}
